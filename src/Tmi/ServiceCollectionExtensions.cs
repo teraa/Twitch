c9e9b711d9ca927ad89e3550383c646eed025c8f
Tera <24725862+teraa@users.noreply.github.com>
2022-04-19 22:36:36 +0200

    add extensions

---
+++ src/Tmi/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Teraa.Twitch.Tmi;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTmiService(this IServiceCollection services)
+    {
+        services.AddSingleton<IClient, WsClient>();
+        services.AddSingleton<TmiServiceOptions>();
+        services.AddSingleton<TmiService>();
+        services.AddHostedService(sp => sp.GetRequiredService<TmiService>());
+        return services;
+    }
+}

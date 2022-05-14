485431c99ad0c04add0abc60875f56a131154dcb
Tera <24725862+teraa@users.noreply.github.com>
2022-05-14 15:03:40 +0200

    add topic types

---
+++ src/PubSub/Topics/Topic.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Teraa.Twitch.PubSub.Topics;
+
+public interface ITopic
+{
+    string Value { get; }
+}
+
+public record ChatModeratorActionsTopic(string UserId, string ChannelId) : ITopic
+{
+    public const string Name = "chat_moderator_actions";
+
+    public string Value => $"{Name}.{UserId}.{ChannelId}";
+}
+
+public record ChannelUnbanRequestsTopic(string UserId, string ChannelId) : ITopic
+{
+    public const string Name = "channel-unban-requests";
+
+    public string Value => $"{Name}.{UserId}.{ChannelId}";
+}
+
+public static class Topic
+{
+    public static bool TryParse(string input, [NotNullWhen(true)] out ITopic? topic)
+    {
+        var parts = input.Split('.');
+
+        topic = parts[0] switch
+        {
+            ChatModeratorActionsTopic.Name when parts.Length == 3 => new ChatModeratorActionsTopic(parts[1], parts[2]),
+            ChannelUnbanRequestsTopic.Name when parts.Length == 3 => new ChannelUnbanRequestsTopic(parts[1], parts[2]),
+            _ => null,
+        };
+
+        return topic is not null;
+    }
+}

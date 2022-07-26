cf32425a9b086552e0770bc95b9cb75a5d11b142
Tera <24725862+teraa@users.noreply.github.com>
2022-07-26 20:43:32 +0200

    helix example

---
+++ test/ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Teraa.Twitch.Helix;
+using Teraa.Twitch.Helix.Users;
+
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var services = new ServiceCollection()
+    .AddLogging(configure => configure.AddSerilog())
+    .AddHelixService(options =>
+    {
+        options.ClientId = config["Twitch:ClientId"];
+    })
+    .BuildServiceProvider(new ServiceProviderOptions
+    {
+        ValidateScopes = true,
+        ValidateOnBuild = true,
+    });
+
+var sc = new ServiceCollection();
+
+using var scope = services.CreateScope();
+var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+var response = await mediator.Send(new Get.Query(Token: config["Twitch:Token"], Logins: new[] {"tera_"}));
+
+_ = response;

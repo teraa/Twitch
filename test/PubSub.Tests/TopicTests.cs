64f4574a16415a70b05c04006cbe84ace20d0d5f
Tera <24725862+teraa@users.noreply.github.com>
2023-11-25 17:26:05 +0100

    add topic tests

---
+++ test/PubSub.Tests/TopicTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Teraa.Twitch.PubSub.Topics;
+using Xunit;
+
+namespace PubSub.Tests;
+
+public class TopicTests
+{
+    private static IEnumerable<object[]> Parse_Topic_Data() => new[]
+    {
+        new object[] {"chat_moderator_actions.user_id.channel_id", new ChatModeratorActionsTopic("user_id", "channel_id")},
+        new object[] {"channel-unban-requests.user_id.channel_id", new ChannelUnbanRequestsTopic("user_id", "channel_id"),},
+        new object[] {"shoutout.channel_id", new ShoutoutTopic("channel_id")},
+    };
+
+    [Theory]
+    [MemberData(nameof(Parse_Topic_Data))]
+    public void Parse_Topic(string input, ITopic expectedTopic)
+    {
+        var success = Topic.TryParse(input, out var topic);
+
+        success.Should().BeTrue();
+        topic.Should().Be(expectedTopic);
+    }
+}

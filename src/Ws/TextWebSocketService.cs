ff69f8778858cb23434b89237409077655ecfeb4
Tera <24725862+teraa@users.noreply.github.com>
2025-04-27 23:10:45 +0200

    add writer and writer queue

--- src/Ws/TextWebSocketService.cs
+++ src/Ws/TextWebSocketService.cs
@@ -1,3 +1,4 @@
+using System.Threading.Channels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,10 +18,12 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
     private readonly TextWebSocketServiceOptions _options;
     private readonly ILogger<TextWebSocketService> _logger;
     private readonly IServiceScopeFactory _scopeFactory;
-    private Task? _readerTask;
     private CancellationTokenSource? _stoppingCts;
-    private readonly SemaphoreSlim _reconnectSem = new(1, 1);
+    private CancellationTokenSource? _reconnectCts;
+    private TaskCompletionSource _connectedTcs;
     private readonly Channel<string> _sendChannel;
+    private Task? _connectorTask;
+    private int _connectCount;
 
     public TextWebSocketService(
         ITextWebSocketClient client,
@@ -32,6 +35,17 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
         _options = options.Value;
         _logger = logger;
         _scopeFactory = scopeFactory;
+        _sendChannel = Channel.CreateUnbounded<string>(
+            new UnboundedChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = true,
+            }
+        );
+
+        // This needs to be a field so that StartAsync can wait until the connection establishes.
+        // Otherwise, it could have been a local variable passed around to other methods.
+        _connectedTcs = new TaskCompletionSource();
     }
 
     // Sends message directly, bypassing the queue.
@@ -43,46 +57,107 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
         await _client.SendAsync(message, cancellationToken);
     }
 
-    private async Task Reconnect(CancellationToken stoppingToken)
+    // Should only have one caller
+    private async Task Connector(CancellationToken stoppingToken)
     {
+        await Task.Yield();
+
+        // We can start writer right away because it will wait for connected TCS to complete before writing.
+        var writerTask = Writer(stoppingToken);
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            await _reconnectSem.WaitAsync(stoppingToken);
             try
             {
-                await ReconnectInternal(stoppingToken);
-                break;
+                // Connect
+                await _client.ConnectAsync(_options.Uri, stoppingToken);
+
+                // Create and save the reconnect CTS before starting the Reader task which could cancel it.
+                // Theoretically, the Writer task could have tried to use it already since we started it already,
+                // but it can't since it will have blocked on waiting for connected TCS to complete.
+                _reconnectCts = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
+
+                // Store the tasks we're executing
+                // We assume these tasks yield immediately and never throw
+                var readerTask = Reader(_reconnectCts.Token);
+
+                // Connect succeeded, invoke and await connected event handlers
+                // We await here because we want to signal to the writer only after the connected handlers run.
+                // That way the client can send messages before the writer resumes consuming the send queue.
+                await InvokeAsync(new ConnectedEvent(this, _connectCount++), stoppingToken);
+
+                // Signal to the writer that it can resume consuming the send queue.
+                // Also signal to the StartAsync that it can return, in case this is the first connection attempt.
+                _connectedTcs.TrySetResult();
+
+
+                // Wait for something to call BeginReconnect
+                try
+                {
+                    await Task.Delay(-1, _reconnectCts.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    // ignore
+                }
+                if (stoppingToken.IsCancellationRequested)
+                    break;
+
+
+                // Reconnect:
+                // If we got here, it means something requested the reconnect,
+                // so we will begin closing procedure before reconnecting
+
+                // Writer will observe a new TCS which will stop it from sending data until we reconnect.
+                _connectedTcs = new TaskCompletionSource();
+
+                // This task is completed already since the cancel token that was passed to it was cancelled
+                // if we got to this point
+                await readerTask.WaitAsync(stoppingToken).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+            }
+            catch (OperationCanceledException)
+            {
+                // ignored
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error reconnecting");
+                _logger.LogError(ex, "Error in connector task");
             }
-            finally
-            {
-                _reconnectSem.Release();
-            }
+        }
+
+        // If we get here, it means stoppingToken was cancelled so the writer task here is done already.
+        await writerTask.ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+    }
+
+    private async Task BeginReconnect(RequestSource source, CancellationToken stoppingToken)
+    {
+        // log who requested reconnect
+        _logger.LogDebug("Reconnect request from {Source}", source);
+
+        // First close then cancel the token. This will try to gracefully close the WebSocket by
+        // sending the close frame first. If we cancel first, the reader task will abort the WebSocket
+        // and prevent us from sending the close frame.
+        // CloseAsync method doesn't throw unless cancelled.
+        try
+        {
+            await _client.CloseAsync(stoppingToken);
+        }
+        catch
+        {
+            // ignored
+        }
+
+        // cancel connection CTS
+        try
+        {
+            _reconnectCts!.Cancel();
+        }
+        catch
+        {
+            // ignored
         }
     }
 
-    private async Task ReconnectInternal(CancellationToken stoppingToken)
-    {
-        if (stoppingToken.IsCancellationRequested)
-            return;
-
-        // We can only get here from reader task itself,
-        // it is guaranteed that it's not null here and there is no race conditions or concurrent access.
-        // This task should be completed at this point since we call reconnect on the exit of read task only.
-        await _readerTask!.WaitAsync(stoppingToken).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
-
-        await _client.CloseAsync(stoppingToken);
-
-        // Connect then store the reader task same as in StartAsync
-        await _client.ConnectAsync(_options.Uri, stoppingToken);
-        _readerTask = Reader(stoppingToken);
-
-        // Reconnect succeeded, invoke reconnected event handlers
-        _ = InvokeAsync(new ReconnectEvent(this), stoppingToken);
-    }
 
     private async Task InvokeAsync<TEvent>(TEvent evt, CancellationToken cancellationToken)
         where TEvent : ITextWebSocketEvent
@@ -136,7 +211,7 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
         catch (Exception ex)
         {
             _logger.LogError(ex, "Error in reader task");
-            _ = Reconnect(stoppingToken);
+            await BeginReconnect(RequestSource.Reader, stoppingToken);
         }
     }
 
@@ -149,19 +224,60 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
             if (result.Type is TextWebSocketReceiveResultType.ClosedUnexpectedly)
             {
                 _logger.LogInformation("WebSocket closed unexpectedly");
-                _ = Reconnect(stoppingToken);
+                await BeginReconnect(RequestSource.Reader, stoppingToken);
                 break;
             }
 
             if (result.Type is TextWebSocketReceiveResultType.CloseMessageReceived)
             {
                 _logger.LogInformation("Received close message");
-                _ = Reconnect(stoppingToken);
+                await BeginReconnect(RequestSource.Reader, stoppingToken);
                 break;
             }
 
             _logger.LogInformation("Received message");
             _ = InvokeAsync(new MessageReceivedEvent(this, result.Message!), stoppingToken);
+        }
+    }
+
+    public void EnqueueMessage(string message)
+    {
+        // This will always succeed for an unbounded channel.
+        _sendChannel.Writer.TryWrite(message);
+    }
+
+    private async Task Writer(CancellationToken stoppingToken)
+    {
+        await Task.Yield();
+
+        await foreach (var message in _sendChannel.Reader.ReadAllAsync(stoppingToken))
+        {
+            _logger.LogTrace("Sending: {Message}", message);
+
+            // Repeatedly try to send the same message until we succeed
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // If send fails, we need to retry sending this message after reconnect,
+                // unless its cancel exception.
+                try
+                {
+                    // Wait until the client is connected
+                    await _connectedTcs.Task;
+                    await _client.SendAsync(message, stoppingToken);
+                    break;
+                }
+                catch (OperationCanceledException)
+                {
+                    // Either the connect TCS or the stopping token was cancelled, in any case we should break out.
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    // Reconnect and retry
+                    _logger.LogError(ex, "Error sending message");
+                    await BeginReconnect(RequestSource.Writer, stoppingToken);
+                }
+            }
         }
     }
 
@@ -181,10 +297,8 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
         // Create linked token to allow cancelling executing task from provided token
         _stoppingCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
-        await _client.ConnectAsync(_options.Uri, cancellationToken);
-
-        // Store the task we're executing
-        _readerTask = Reader(_stoppingCts.Token);
+        _connectorTask = Connector(_stoppingCts.Token);
+        await _connectedTcs.Task;
 
         _logger.LogInformation("Started");
     }
@@ -192,21 +306,18 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         // Stop called without start
-        if (_readerTask == null)
+        if (_connectorTask == null)
         {
             return;
         }
 
         try
         {
-            // First close then cancel the token. This will try to gracefully close the WebSocket by
-            // sending the close frame first. If we cancel first, the reader task will abort the WebSocket
-            // and prevent us from sending the close frame.
             await _client.CloseAsync(cancellationToken);
         }
-        catch (Exception ex)
+        catch
         {
-            _logger.LogError(ex, "Error closing client");
+            // ignored
         }
 
         try
@@ -219,7 +330,9 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
             // ignored
         }
 
-        await _readerTask.WaitAsync(cancellationToken).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+        _connectedTcs.TrySetCanceled();
+
+        await _connectorTask.WaitAsync(cancellationToken).ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
 
         _logger.LogInformation("Stopped");
     }
@@ -235,10 +348,17 @@ public sealed class TextWebSocketService : IHostedService, ITextWebSocketService
             // ignored
         }
 
+
         _client.Dispose();
         _stoppingCts?.Dispose();
-        _reconnectSem.Dispose();
-        _readerTask = null;
+        _reconnectCts?.Dispose();
+        _connectorTask = null;
+    }
+
+    private enum RequestSource
+    {
+        Reader,
+        Writer,
     }
 }
 
@@ -247,8 +367,9 @@ public interface ITextWebSocketEvent
     ITextWebSocketService Service { get; }
 }
 
-public sealed record ReconnectEvent(
-    ITextWebSocketService Service
+public sealed record ConnectedEvent(
+    ITextWebSocketService Service,
+    int ConnectCount
 ) : ITextWebSocketEvent;
 
 public sealed record MessageReceivedEvent(
